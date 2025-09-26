e91a4a906033ac02ffd882d479bc5a29948dc9d7
kculley <kculley@ut.utm.edu>
2025-09-25 19:27:39 -0500

    idk

--- main.cpp
+++ main.cpp
@@ -4,20 +4,23 @@
 
 int main(){
 
-const int screenWidth = 1240;
-const int screenHeight = 720;
+const int screenWidth = 3840;
+const int screenHeight = 2160;
 InitWindow(screenWidth,screenHeight,"Test Window For GUI");
 
 SetTargetFPS(60);
 
-Rectangle button1 = {300,300,500,200};
+Rectangle button1 = {20,20,screenWidth/6,150};
+Rectangle button2 = {20,190,screenWidth/6,150};
 
 while(!WindowShouldClose()){
     Vector2 mousePos = GetMousePosition();
 
+    //First Button is start scan from raspberryPI
     if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
         if(CheckCollisionPointRec(mousePos, button1)){
             std::cout << "I clicked the button" << std::endl;
+            //script.sendToRaspberryPi (runScanProgram)
         }
     }
 
@@ -27,7 +30,7 @@ while(!WindowShouldClose()){
     ClearBackground(WHITE);
 
     DrawRectangleRec(button1,PURPLE);
-
+    DrawRectangleRec(button2,GRAY);
     EndDrawing();
     }
 
